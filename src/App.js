24d065b10bdb40731e2ef17f1d1a99722c3b897c
Martin Szlapa <szlapamartin@gmail.com>
2023-11-21 14:56:05 -0500

    initial commit part 2

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Home/Home';
+
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+
+            <Home/>
+        </div>
+    );
+}
+
+export default App;
